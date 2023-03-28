12e1bd2633cf639e5cbada56b1450512224a198a
mohamed-622 <mohamedezghoudi@gmail.com>
2023-03-27 17:35:57 -0700

    fixing errors

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main.h
+#define main.h
 
 #include <stdio.h>
 
@@ -15,4 +15,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif /* main.h */
