369e6599ef9fb027c62007381aa69b094a49484b
Mohamed <mohamedezghoudi@gmail.com>
2023-05-06 01:30:56 +0100

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * This function, binary_to_uint, takes a string of binary digits as input and 
+ * returns the equivalent unsigned integer value of the binary number.
+ *
+ * @b: A string containing the binary number.
+ *
+ * Return: The converted unsigned integer value of the binary number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
+
+	return (dec_val);
+}
